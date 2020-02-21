33c5102facb26a55e787bbd768397aae186c3bbe
James Souksavanh Moua <Smoua1558@gmail.com>
2020-02-21 02:16:24 -0500

    Rough draft of one or two state search results

--- index.js
+++ index.js
@@ -2,26 +2,37 @@ const apiKey1 = 'tnEaorvnanlGBb2WdJ8fTfhggAr99U6cXt3cbCV9'; //Brandons APIkey
 const apiKey2 = 'bTiRnlLq5XLzUqSykpwS0yNsOcbmqxJV9YaOuA3y'; //James APIkey
 const SEARCH_URL = 'https://developer.nps.gov/api/v1/parks'; //API source
 
-function callAPI(inputVal) {
-  fetch(`${SEARCH_URL}?stateCode=${inputVal}&api_Key=${apiKey2}`)
-    .then(response => response.json())
-    .then(newResponse => displayResults(newResponse))
-    .catch(error => console.log(error));
+//Calls to API
+function callAPI(inputVal, maxResults) {
+  for(let i = 0; i < maxResults; i++) {
+    fetch(`${SEARCH_URL}?stateCode=${inputVal}&api_Key=${apiKey2}`)
+      .then(response => response.json())
+      .then(newResponse => displayResults(newResponse, i))
+      .catch(error => console.log(error));
+  }
 }
 
+//Click Search and receive data
 function submitButton() {
-  $('.form').submit(function(event){
+  $('.form').on('click', '#search', event => {
     event.preventDefault();
-    let inputVal = $('#search-state-1').val();
-    //let inputVal2 = $('#search-state-2').val();
-    callAPI(inputVal);
+    createContent();
   });
 }
 
-function displayResults(newResponse){
-  let name = newResponse.data[0].fullName;
-  let descript = newResponse.data[0].description;
-  let link = newResponse.data[0].url;
+function createContent(){
+  for(let i = 0; i < 2; i++) {
+    let inputVal = $(`#search-state-${i}`).val();
+    //let inputVal2 = $('#search-state-2').val();
+    let maxResults = $(`#np-max-results-${i}`).val();
+    callAPI(inputVal, maxResults);
+  }
+}
+
+function displayResults(newResponse, i){
+  let name = newResponse.data[i].fullName;
+  let descript = newResponse.data[i].description;
+  let link = newResponse.data[i].url;
 
   $('.results').append(`
   <h3>${name}</h3>
@@ -29,5 +40,4 @@ function displayResults(newResponse){
   <a href="${link}">${link}</a>
   `);
 }
-
 submitButton();
