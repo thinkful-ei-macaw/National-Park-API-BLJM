040b7cf39aa08c6393252fcb02f5d56cc8c63934
James Souksavanh Moua <Smoua1558@gmail.com>
2020-02-21 00:25:21 -0500

    created form and submitButton function

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
 const apiKey1 = 'tnEaorvnanlGBb2WdJ8fTfhggAr99U6cXt3cbCV9'; //Brandons APIkey
-const apiKey2 = ''; //James APIkey
+const apiKey2 = 'bTiRnlLq5XLzUqSykpwS0yNsOcbmqxJV9YaOuA3y'; //James APIkey
 const SEARCH_URL = 'https://developer.nps.gov/api/v1/alerts'; //API source
 
-
-function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-  return queryItems.join('&');
-}
-
-function getResults(state, result = 10){
-  //set default param to 10
-  //make obj with api, state, limit properties
-  //uses fetch and then promises or throws error
-}
-
-function resultDisplay(responseJson){
-  //loops thru Natl Park API and maps search results
-}
-
-function formSubmit(){
-  //event listeners for search term form submission
-  //variables hooked to search-terms in HTML forms that get the values
+function callAPI() {
 
 }
 
-function resetForms(){
-  //event listeners for reset button
+function submitButton() {
+  $('.form').submit(function(event){
+    event.preventDefault();
+    let inputVal = $('#search-state-1').val();
+    let inputVal2 = $('#search-state-2').val();
+    console.log(inputVal);
+    console.log(inputVal2);
+  });
 }
+
+submitButton();
