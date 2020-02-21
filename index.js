0eb695be1458786b3fa0f459998b36e67e1d27f3
James Souksavanh Moua <Smoua1558@gmail.com>
2020-02-21 00:53:03 -0500

    created a div that appends a template one park with full name, description, and url

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const apiKey1 = 'tnEaorvnanlGBb2WdJ8fTfhggAr99U6cXt3cbCV9'; //Brandons APIkey
 const apiKey2 = 'bTiRnlLq5XLzUqSykpwS0yNsOcbmqxJV9YaOuA3y'; //James APIkey
-const SEARCH_URL = 'https://developer.nps.gov/api/v1/alerts'; //API source
+const SEARCH_URL = 'https://developer.nps.gov/api/v1/parks'; //API source
 
-function callAPI() {
-
+function callAPI(inputVal) {
+  fetch(`${SEARCH_URL}?stateCode=${inputVal}&api_Key=${apiKey2}`)
+    .then(response => response.json())
+    .then(newResponse => displayResults(newResponse))
+    .catch(error => console.log(error));
 }
 
 function submitButton() {
   $('.form').submit(function(event){
     event.preventDefault();
     let inputVal = $('#search-state-1').val();
-    let inputVal2 = $('#search-state-2').val();
-    console.log(inputVal);
-    console.log(inputVal2);
+    //let inputVal2 = $('#search-state-2').val();
+    callAPI(inputVal);
   });
+}
+
+function displayResults(newResponse){
+  let name = newResponse.data[0].fullName;
+  let descript = newResponse.data[0].description;
+  let link = newResponse.data[0].url;
+
+  $('.results').append(`
+  <h3>${name}</h3>
+  <p>${descript}</p>
+  <a href="${link}">${link}</a>
+  `);
 }
 
 submitButton();
